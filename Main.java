987c41503ac90b7e60fa4a0f2b12f9c4ebdb1356
r1ddh118 <riddhipoddar@gmail.com>
2025-04-13 14:58:23 +0530

    main file is added

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Main.java
+// Name: Riddhi Poddar
+// PRN: 23070126105
+// Batch: 2023 - 27 (B2 Batch)
+
+public class Main {
+    public static void main(String[] args) {
+        UserInput userInput = new UserInput();
+        userInput.selectOperation();
+    }
+}
+
